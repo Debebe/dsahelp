f390528ef2d85157f2fbf48bad889f4cce3f5f4c
Debebe Adewo <debebesh@gmail.com^c>
2023-11-23 09:51:57 +0000

    separate_header is becoming obsolete, so using split header function from ftExtra
    In the updated flextable, the headerline is missing and I am using flextable::hline(i = 1, part = 'header') to counter that. I will check in the next iterations

--- R/script.R
+++ R/script.R
@@ -1,12 +1,6 @@
-#
-# Some useful keyboard shortcuts for package authoring:
-#
-#   Install Package:           'Cmd + Shift + B'
-#   Check Package:             'Cmd + Shift + E'
-#   Test Package:              'Cmd + Shift + T'
 
 #' Title Create publication quality table
-#' @param data data.frame for tabble creation
+#' @param data Data.frame for table creation
 #' @import ftExtra, officer, flextable
 #' @return
 #' @export
@@ -16,19 +10,25 @@ create_table <- function(data){
   ft <- comb%>%
     as_flextable(hide_grouplabel = TRUE)%>%
     padding(padding = 0, part = "all") %>%
-    separate_header()%>%
-    span_header()%>%
+    #separate_header()%>%
+    ftExtra::split_header()%>%
+    ftExtra::span_header()%>%
+    flextable::hline(i = 1, part = 'header')%>%
     autofit()
   # bolding the grouping variable
   ft <- ft%>%
     bold(j = 1, i = ~ !is.na(Category), bold = TRUE, part = "body")
-  ft <- align(ft, i = 1, NULL, align = "center", part = "header")
+
+  ft <- align(ft, i = 1, j=-1,  align = "center", part = "header")
+  ft <- align(ft, i = 2, j=-1, align = "center", part = "header")
+  ft <- align(ft, j = -1, align = "center", part = "body")
 
   ft <- padding(ft, padding.top = 0, part = "header")
   ft <- padding(ft, padding.bottom = 0, part = "header")
   ft <- bold(ft, bold = TRUE, part = "header")
   ft <- font(ft, fontname = "Arial", part = "header")
   ft <- fontsize(ft, i = NULL, j = NULL, size = 9, part = "body")
+  ft <- fontsize(ft, i = NULL, j = NULL, size = 9, part = "header")
 
   # Adding footer
   ft <-add_footer_lines(ft, "") %>%
