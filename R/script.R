68a52db2f1fe1174663c5686e5b0514ccff5a844
Debebe Adewo <debebesh@gmail.com^c>
2023-11-23 10:13:30 +0000

    Removing redundat lines of code

--- R/script.R
+++ R/script.R
@@ -1,5 +1,5 @@
 
-#' Title Create publication quality table
+#' Title Create publication quality tables
 #' @param data Data.frame for table creation
 #' @import ftExtra, officer, flextable
 #' @return
@@ -18,20 +18,17 @@ create_table <- function(data){
   # bolding the grouping variable
   ft <- ft%>%
     bold(j = 1, i = ~ !is.na(Category), bold = TRUE, part = "body")
-
-  ft <- align(ft, i = 1, j=-1,  align = "center", part = "header")
-  ft <- align(ft, i = 2, j=-1, align = "center", part = "header")
-  ft <- align(ft, j = -1, align = "center", part = "body")
+  ft <- align(ft, j=-1,  align = "center", part = "header")
+  ft <- align(ft, j=-1, align = "center", part = "body")
 
   ft <- padding(ft, padding.top = 0, part = "header")
   ft <- padding(ft, padding.bottom = 0, part = "header")
   ft <- bold(ft, bold = TRUE, part = "header")
   ft <- font(ft, fontname = "Arial", part = "header")
-  ft <- fontsize(ft, i = NULL, j = NULL, size = 9, part = "body")
-  ft <- fontsize(ft, i = NULL, j = NULL, size = 9, part = "header")
+  ft <- fontsize(ft, i = NULL, j = NULL, size = 9, part = "all")
 
   # Adding footer
-  ft <-add_footer_lines(ft, "") %>%
+  ft <-add_footer_lines(ft, "NB") %>%
     font(part = "footer", fontname = "Arial") %>%
     bold(bold = FALSE, part = "footer")
   # adding font size for footer
